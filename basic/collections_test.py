4744eab7a24d384e6c1e1cfc5ae045cadc29fee5
imkyo <imkyo.jung@iqair.com>
2024-11-27 19:29:00 +0900

    add basic

--- basic/collections_test.py
+++ basic/collections_test.py
@@ -1,6 +1,14 @@
 # Counter, namedtuple, OrderedDict, defaultdict, deque
-from collections import Counter
+from collections import defaultdict, Counter
 a = 'aaaabbbbbcccc'
 
 my_counter = Counter(a)
 print(my_counter)
+
+
+# 创建一个带默认值的字典
+my_defaultdict = defaultdict(list)
+
+# 访问不存在的键时，自动创建默认值
+my_defaultdict['key'].append(1)
+print(my_defaultdict)  # 输出: defaultdict(<class 'list'>, {'key': [1]})
